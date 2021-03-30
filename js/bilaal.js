5e3dffe84cf4fe14155b9bfc5082492c0f373f60
MrPakoras <hartanas8g@gmail.com>
2021-03-30 10:59:42 +0100

    Updated Progress

--- js/bilaal.js
+++ js/bilaal.js
@@ -8,13 +8,13 @@ document.getElementById('anime_name').innerHTML = '\xa0' + 'One Piece' + '\xa0';
 // root.style.setProperty('--colour_highlight','#00ff91');
 // root.style.setProperty('--colour_text','#6af4ba');
 
-var start_date = new Date('03/24/2021').getTime(); // Edit this
+var start_date = new Date('03/30/2021').getTime(); // Edit this
 var init_start_date = new Date('Apr 15 2020').getTime();
 
 var now = new Date().getTime();
 var time_since = now - start_date;
 
-var eps_watched = 47; // Edit this
+var eps_watched = 77; // Edit this
 document.getElementById('eps_watched_text').innerHTML = '\xa0' + eps_watched + '\xa0';
 
 var eps_total = 952;
